4827c3f2ae9821e30d16db108b1f83bcb8aca3c5
Naveed_Madikeri_Umar <navid.nony@gmail.com>
2024-06-29 15:33:54 +0530

    Finish adding UserTl

--- SpringMVC/src/main/java/com/akhm/dao/model/UserTl.java
+++ SpringMVC/src/main/java/com/akhm/dao/model/UserTl.java
@@ -3,56 +3,87 @@ package com.akhm.dao.model;
 import java.io.Serializable;
 import java.sql.Date;
 
-public class UserTl implements Serializable{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "USER_TL")
+public class UserTl implements Serializable {
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer userId;
+	@Column(name = "FIRST_NAME", nullable = false, length = 50)
 	private String firstName;
+	@Column(name = "LAST_NAME", nullable = false, length = 50)
 	private String lastName;
+	@Column(name = "EMAIL_ID", nullable = false, length = 50)
 	private String emailId;
+	@Column(name = "MOBILE_NUMBER", nullable = false, length = 50)
 	private String mobileNumber;
+	@Column(name = "PASSWORD", nullable = false, length = 50)
 	private String passWord;
+	@Column(name = "DATE_OF_BIRTH", nullable = false)
 	private Date dob;
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public String getPassWord() {
 		return passWord;
 	}
+
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
 	}
+
 	public Date getDob() {
 		return dob;
 	}
+
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
-	
+
 }
