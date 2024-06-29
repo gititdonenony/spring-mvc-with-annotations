7eef2ec755af4c8195fe59c315555fb9058d98ae
Naveed_Madikeri_Umar <navid.nony@gmail.com>
2024-06-29 16:39:38 +0530

    Microservices to be continued

--- SpringMVC/src/main/java/com/akhm/controller/UserController.java
+++ SpringMVC/src/main/java/com/akhm/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.akhm.controller.command.UserCommand;
+import com.akhm.dto.UserDTO;
 import com.akhm.service.UserService;
 
 @Controller
@@ -15,5 +17,25 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.GET, value = "/adduser.htm")
 	public String showAddUser() {
 		return "adduser.jsp";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/adduser.htm")
+	public String addUser(UserCommand userCommand) {
+		String target = "adduser";
+		UserDTO userDTO = new UserDTO();
+		userDTO.setFirstName(userCommand.getFirstName());
+		userDTO.setLastName(userCommand.getLastName());
+		userDTO.setEmailId(userCommand.getEmailId());
+		userDTO.setMobileNumber(userCommand.getMobileNumber());
+		userDTO.setPassword(userCommand.getPassword());
+		userDTO.setDob(userCommand.getDob());
+		if (userCommand.getDob() != null) {
+
+		}
+		Integer userId = userService.saveUser(userDTO);
+		if (userId != null && userId > 0) {
+			target = "redirect:users.htm";
+		}
+		return target;
 	}
 }
